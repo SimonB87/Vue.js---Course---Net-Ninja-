c41cd357f18e61af17b8090dd084d620f27c2598
SimonB87 <buryans@gmail.com>
2020-04-01 00:05:20 +0200

    Vue2.js blank template

--- app.js
+++ app.js
@@ -1,43 +1,10 @@
-const oneInstance = new Vue({
-    el: '#vue-app-one',
+new Vue({
+    el: '#vue-app',
     data: {
-        title: "Vue App One",
+
     },
     methods: {
         
-    },
-    computed: {
-        greet: function(){
-            return "Hello from " + this.title;
-        }
-    }
-});
-
-const twoInstance = new Vue({
-    el: '#vue-app-two',
-    data: {
-        title: "Vue App Two",
-    },
-    methods: {
-        changeTitle: function(){
-            oneInstance.title = "Title changed by " + this.title;
-        }
-    },
-    computed: {
-        greet: function(){
-            return "Cheers from " + this.title;
-        }
-    }
-});
-
-const threeInstance = new Vue({
-    el: '#vue-app-three',
-    data: {
-        title: "Vue App three",
-        name: ""
-    },
-    methods: {
-    
     },
     computed: {
 
