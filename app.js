bbbf2e65e31ff29f59e19a57bd745ee3ff6b7047
SimonB87 <buryans@gmail.com>
2020-04-04 22:02:29 +0200

    Lesson-16 - refs

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 new Vue({
     el: '#vue-app',
     data: {
-
+        outputValue: "Your fav food"
     },
     methods: {
-        
+        readRefs: function(){
+            console.log("*** *** ***");
+
+            // ref of all
+            console.log(this.$refs);
+            // ref of ref="input"
+            console.log(this.$refs.input);
+            console.log(this.$refs.input.value);
+            // ref of ref="test"
+            console.log(this.$refs.test.innerText);
+
+            console.log("*** *** ***");
+
+            // change 
+            this.outputValue = this.$refs.input.value;
+
+
+        }
     },
     computed: {
 
